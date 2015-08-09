bb4bc70987d107c65623dafce26e9303f4b2bcc4
初コミット嬉し恥ずかしいでちゅ <r0cknet@r0ckNethikar1-no-MacBook-Pro.local>
2015-08-09 14:14:12 +0900

    14:14現在の状態をコミット

--- osaka/joympics/app/controllers/supporters_controller.rb
+++ osaka/joympics/app/controllers/supporters_controller.rb
@@ -1,10 +1,11 @@
 class SupportersController < ApplicationController
 
   def index
-
     @supporters=Supporter.all.limit(5)
 
-
+    # @supporters.each do |supporter|
+    #   render :json => supporter
+    # end
   end
 
 end
