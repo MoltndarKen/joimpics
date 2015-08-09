8c78e1cffa102905a825b9e02b4e78f467f7b4b1
asunaronoki <asunaronoki.ct@gmail.com>
2015-08-09 14:07:38 +0900

    Commit Test

---
+++ osaka/dummy_seeds.rb
@@ -0,0 +1,151 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+@suppoter = Suppoter.new
+@suppoter.name = 'Jacob'
+@suppoter.lang = 'en'
+@suppoter.sport = 'run100m'
+@suppoter.athlete = 'Bolt'
+@suppoter.long = '139.71759450000002'
+@suppoter.lat = '35.6787954'
+@suppoter.save
+
+@suppoter = Suppoter.new
+@suppoter.name = 'Jackson'
+@suppoter.lang = 'fr'
+@suppoter.sport = 'run100m'
+@suppoter.athlete = 'Shirai'
+@suppoter.long = '139.728792'
+@suppoter.lat = '35.6801453'
+@suppoter.save
+
+@suppoter = Suppoter.new
+@suppoter.name = 'Noah'
+@suppoter.lang = 'ja'
+@suppoter.sport = 'run100m'
+@suppoter.athlete = 'Bolt'
+@suppoter.long = '139.7100517'
+@suppoter.lat = '35.68517629999999'
+@suppoter.save
+
+@suppoter = Suppoter.new
+@suppoter.name = 'Lucas'
+@suppoter.lang = 'fr'
+@suppoter.sport = 'run200m'
+@suppoter.athlete = 'Kitajima'
+@suppoter.long = '139.75616309999998'
+@suppoter.lat = '35.6738235'
+@suppoter.save
+
+@suppoter = Suppoter.new
+@suppoter.name = 'Sophia'
+@suppoter.lang = 'en'
+@suppoter.sport = 'run200m'
+@suppoter.athlete = 'Bolt'
+@suppoter.long = '139.7100517'
+@suppoter.lat = '35.68517629999999'
+@suppoter.save
+
+@suppoter = Suppoter.new
+@suppoter.name = 'Chloe'
+@suppoter.lang = 'fr'
+@suppoter.sport = 'run200m'
+@suppoter.athlete = 'Nishikori'
+@suppoter.long = '139.7100517'
+@suppoter.lat = '35.6787954'
+@suppoter.save
+
+@suppoter = Suppoter.new
+@suppoter.name = 'Abigail'
+@suppoter.lang = 'ja'
+@suppoter.sport = 'run200m'
+@suppoter.athlete = 'Bolt'
+@suppoter.long = '139.71759450000002'
+@suppoter.lat = '35.6787954'
+@suppoter.save
+
+@suppoter = Suppoter.new
+@suppoter.name = 'Anne'
+@suppoter.lang = 'en'
+@suppoter.sport = 'swim'
+@suppoter.athlete = 'Shirai'
+@suppoter.long = '139.69430280000006'
+@suppoter.lat = '35.6852321'
+@suppoter.save
+
+@suppoter = Suppoter.new
+@suppoter.name = 'John'
+@suppoter.lang = 'ja'
+@suppoter.sport = 'swim'
+@suppoter.athlete = 'Gatlin'
+@suppoter.long = '139.73000000000002'
+@suppoter.lat = '35.66'
+@suppoter.save
+
+@suppoter = Suppoter.new
+@suppoter.name = 'Jack'
+@suppoter.lang = 'en'
+@suppoter.sport = 'swim'
+@suppoter.athlete = 'Shirai'
+@suppoter.long = '139.74506900000006'
+@suppoter.lat = '35.662978'
+@suppoter.save
+
+@suppoter = Suppoter.new
+@suppoter.name = 'Sara'
+@suppoter.lang = 'fr'
+@suppoter.sport = 'swim'
+@suppoter.athlete = 'Bolt'
+@suppoter.long = '139.71759450000002'
+@suppoter.lat = '35.680059'
+@suppoter.save
+
+@suppoter = Suppoter.new
+@suppoter.name = 'Sherry'
+@suppoter.lang = 'ja'
+@suppoter.sport = 'soccer'
+@suppoter.athlete = 'Bolt'
+@suppoter.long = '139.71412699999996'
+@suppoter.lat = '35.6896342'
+@suppoter.save
+
+@suppoter = Suppoter.new
+@suppoter.name = 'Bob'
+@suppoter.lang = 'ja'
+@suppoter.sport = 'soccer'
+@suppoter.athlete = 'Nishikori'
+@suppoter.long = '139.71412699999996'
+@suppoter.lat = '35.680059'
+@suppoter.save
+
+@suppoter = Suppoter.new
+@suppoter.name = 'Tom'
+@suppoter.lang = 'en'
+@suppoter.sport = 'soccer'
+@suppoter.athlete = 'Kitajima'
+@suppoter.long = '139.69210069999997'
+@suppoter.lat = '35.6896342'
+@suppoter.save
+
+@suppoter = Suppoter.new
+@suppoter.name = 'Jenny'
+@suppoter.lang = 'ja'
+@suppoter.sport = 'soccer'
+@suppoter.athlete = 'Bolt'
+@suppoter.long = '139.7100517'
+@suppoter.lat = '35.68517629999999'
+@suppoter.save
+
+@suppoter = Suppoter.new
+@suppoter.name = 'Rick'
+@suppoter.lang = 'fr'
+@suppoter.sport = 'run100m'
+@suppoter.athlete = 'Gatlin'
+@suppoter.long = '139.69131800000002'
+@suppoter.lat = '35.668954'
+@suppoter.save
