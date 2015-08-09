c66254dd5a3109d79e4f42f07718c9918a4abdb4
初コミット嬉し恥ずかしいでちゅ <r0cknet@r0ckNethikar1-no-MacBook-Pro.local>
2015-08-09 14:30:23 +0900

    seedを入れたものに対して

--- osaka/joympics/db/seeds.rb
+++ osaka/joympics/db/seeds.rb
@@ -5,3 +5,148 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+@supporter = Supporter.new
+@supporter.name = 'Jacob'
+@supporter.lang = 'en'
+@supporter.sport = 'run100m'
+@supporter.athlete = 'Bolt'
+@supporter.long = '139.71759450000002'
+@supporter.lat = '35.6787954'
+@supporter.save
+
+@supporter = Supporter.new
+@supporter.name = 'Jackson'
+@supporter.lang = 'fr'
+@supporter.sport = 'run100m'
+@supporter.athlete = 'Shirai'
+@supporter.long = '139.728792'
+@supporter.lat = '35.6801453'
+@supporter.save
+
+@supporter = Supporter.new
+@supporter.name = 'Noah'
+@supporter.lang = 'ja'
+@supporter.sport = 'run100m'
+@supporter.athlete = 'Bolt'
+@supporter.long = '139.7100517'
+@supporter.lat = '35.68517629999999'
+@supporter.save
+
+@supporter = Supporter.new
+@supporter.name = 'Lucas'
+@supporter.lang = 'fr'
+@supporter.sport = 'run200m'
+@supporter.athlete = 'Kitajima'
+@supporter.long = '139.75616309999998'
+@supporter.lat = '35.6738235'
+@supporter.save
+
+@supporter = Supporter.new
+@supporter.name = 'Sophia'
+@supporter.lang = 'en'
+@supporter.sport = 'run200m'
+@supporter.athlete = 'Bolt'
+@supporter.long = '139.7100517'
+@supporter.lat = '35.68517629999999'
+@supporter.save
+
+@supporter = Supporter.new
+@supporter.name = 'Chloe'
+@supporter.lang = 'fr'
+@supporter.sport = 'run200m'
+@supporter.athlete = 'Nishikori'
+@supporter.long = '139.7100517'
+@supporter.lat = '35.6787954'
+@supporter.save
+
+@supporter = Supporter.new
+@supporter.name = 'Abigail'
+@supporter.lang = 'ja'
+@supporter.sport = 'run200m'
+@supporter.athlete = 'Bolt'
+@supporter.long = '139.71759450000002'
+@supporter.lat = '35.6787954'
+@supporter.save
+
+@supporter = Supporter.new
+@supporter.name = 'Anne'
+@supporter.lang = 'en'
+@supporter.sport = 'swim'
+@supporter.athlete = 'Shirai'
+@supporter.long = '139.69430280000006'
+@supporter.lat = '35.6852321'
+@supporter.save
+
+@supporter = Supporter.new
+@supporter.name = 'John'
+@supporter.lang = 'ja'
+@supporter.sport = 'swim'
+@supporter.athlete = 'Gatlin'
+@supporter.long = '139.73000000000002'
+@supporter.lat = '35.66'
+@supporter.save
+
+@supporter = Supporter.new
+@supporter.name = 'Jack'
+@supporter.lang = 'en'
+@supporter.sport = 'swim'
+@supporter.athlete = 'Shirai'
+@supporter.long = '139.74506900000006'
+@supporter.lat = '35.662978'
+@supporter.save
+
+@supporter = Supporter.new
+@supporter.name = 'Sara'
+@supporter.lang = 'fr'
+@supporter.sport = 'swim'
+@supporter.athlete = 'Bolt'
+@supporter.long = '139.71759450000002'
+@supporter.lat = '35.680059'
+@supporter.save
+
+@supporter = Supporter.new
+@supporter.name = 'Sherry'
+@supporter.lang = 'ja'
+@supporter.sport = 'soccer'
+@supporter.athlete = 'Bolt'
+@supporter.long = '139.71412699999996'
+@supporter.lat = '35.6896342'
+@supporter.save
+
+@supporter = Supporter.new
+@supporter.name = 'Bob'
+@supporter.lang = 'ja'
+@supporter.sport = 'soccer'
+@supporter.athlete = 'Nishikori'
+@supporter.long = '139.71412699999996'
+@supporter.lat = '35.680059'
+@supporter.save
+
+@supporter = Supporter.new
+@supporter.name = 'Tom'
+@supporter.lang = 'en'
+@supporter.sport = 'soccer'
+@supporter.athlete = 'Kitajima'
+@supporter.long = '139.69210069999997'
+@supporter.lat = '35.6896342'
+@supporter.save
+
+@supporter = Supporter.new
+@supporter.name = 'Jenny'
+@supporter.lang = 'ja'
+@supporter.sport = 'soccer'
+@supporter.athlete = 'Bolt'
+@supporter.long = '139.7100517'
+@supporter.lat = '35.68517629999999'
+@supporter.save
+
+@supporter = Supporter.new
+@supporter.name = 'Rick'
+@supporter.lang = 'fr'
+@supporter.sport = 'run100m'
+@supporter.athlete = 'Gatlin'
+@supporter.long = '139.69131800000002'
+@supporter.lat = '35.668954'
+@supporter.save
